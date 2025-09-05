2585cadf3938e5ac5e29765e37cd1baf988038ba
Jade Thompson-Tavai <jade.t.tavai@gmail.com>
2025-09-05 23:23:22 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const apikey = 'AIzaSyDeYMMUrJfFcUCpIipiQzyE1jcRcKzND-4';
+const apikey = 'AIzaSyDI9vZzyRYNRt8Tf29bAmemX1ngLgXAyU4';
    const useProxy = true;
     const proxy = "https://cors-anywhere.herokuapp.com/";
 
